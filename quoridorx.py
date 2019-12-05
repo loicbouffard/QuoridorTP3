2dcdb85b25034c6b35f618ae4d955d896d0f1b15
Loïc Bouffard <loic.bouffard.1@ulaval.ca>
2019-12-04 23:09:46 -0500

    correection init, construire_graph

--- quoridorx.py
+++ quoridorx.py
@@ -3,24 +3,26 @@ import quoridor
 
 
 class QuoridorX(quoridor.Quoridor):
-    
 
     def __init__(self, joueur, murs=None):
-        self.jeu = quoridor.Quoridor(joueur, murs)
+        super().__init__(joueur, murs)
         self.turtle_j1 = turtle.Turtle()
         self.turtle_j2 = turtle.Turtle()
-        
+
     def afficher(self):
         fen = turtle.Screen()
         fen.title("Quoridor")
         fen.setup(width=700, height=600)
-        pointTab=((-200, 250),(250, 250), (250, -200), (-200, -200), (-200, 250), (-150,250),(-150,-200),
-        (-100,-200),(-100,250),(-50,250),(-50,-200),(0,-200),(0,250),(50,250),(50,-200),(100,-200),
-        (100,250),(150,250),(150,-200),(200,-200),(200,250),(250,250),(250,200),(-200,200),(-200,150),
-        (250,150),(250,100),(-200,100),(-200,50),(250,50),(250,0),(-200,0),(-200,-50),(250,-50),
-        (250,-100),(-200,-100),(-200,-150),(250,-150))
+        pointTab = ((-200, 250), (250, 250), (250, -200), (-200, -200), (-200, 250), (-150, 250), (-150, -200),
+                    (-100, -200), (-100, 250), (-50, 250), (-50, -200), (0, -
+                                                                         200), (0, 250), (50, 250), (50, -200), (100, -200),
+                    (100, 250), (150, 250), (150, -200), (200, -200), (200,
+                                                                       250), (250, 250), (250, 200), (-200, 200), (-200, 150),
+                    (250, 150), (250, 100), (-200, 100), (-200, 50), (250,
+                                                                      50), (250, 0), (-200, 0), (-200, -50), (250, -50),
+                    (250, -100), (-200, -100), (-200, -150), (250, -150))
 
-        tab=turtle.Turtle()
+        tab = turtle.Turtle()
         tab.hideturtle()
         tab.speed(0)
         tab.penup()
@@ -29,20 +31,20 @@ class QuoridorX(quoridor.Quoridor):
         for i in pointTab:
             tab.goto(i)
 
-        mur= turtle.Turtle()
+        mur = turtle.Turtle()
         mur.speed(0)
         mur.hideturtle()
-        fen.addshape('murV',((-45,5),(45,5),(45,-5),(-45,-5)))
+        fen.addshape('murV', ((-45, 5), (45, 5), (45, -5), (-45, -5)))
         mur.shape('murV')
         mur.penup()
-        for i in self.jeu.liste_murs['verticaux']:
-            mur.goto(-200+50*(i[0]-1),-200+50*i[1])
+        for i in self.liste_murs['verticaux']:
+            mur.goto(-200+50*(i[0]-1), -200+50*i[1])
             mur.stamp()
 
-        fen.addshape('murH',((5,-45),(5,45),(-5,45),(-5,-45)))
+        fen.addshape('murH', ((5, -45), (5, 45), (-5, 45), (-5, -45)))
         mur.shape('murH')
-        for i in self.jeu.liste_murs['horizontaux']:
-            mur.goto(-200+50*i[0],-200+50*(i[1]-1))
+        for i in self.liste_murs['horizontaux']:
+            mur.goto(-200+50*i[0], -200+50*(i[1]-1))
             mur.stamp()
 
         self.turtle_j1.hideturtle()
@@ -50,7 +52,8 @@ class QuoridorX(quoridor.Quoridor):
         self.turtle_j1.speed(0)
         self.turtle_j1.penup()
         self.turtle_j1.shape('circle')
-        self.turtle_j1.goto((-175+50*(self.jeu.liste_joueurs[0]['pos'][0]-1),-175+50*(self.jeu.liste_joueurs[0]['pos'][1]-1)))
+        self.turtle_j1.goto(
+            (-175+50*(self.liste_joueurs[0]['pos'][0]-1), -175+50*(self.liste_joueurs[0]['pos'][1]-1)))
         self.turtle_j1.stamp()
 
         self.turtle_j2.hideturtle()
@@ -59,12 +62,15 @@ class QuoridorX(quoridor.Quoridor):
         self.turtle_j2.speed(0)
         self.turtle_j2.penup()
         self.turtle_j2.shape('circle')
-        self.turtle_j2.goto((-175+50*(self.jeu.liste_joueurs[1]['pos'][0]-1),-175+50*(self.jeu.liste_joueurs[1]['pos'][1]-1)))
+        self.turtle_j2.goto(
+            (-175+50*(self.liste_joueurs[1]['pos'][0]-1), -175+50*(self.liste_joueurs[1]['pos'][1]-1)))
         self.turtle_j2.stamp()
 
-jeu= QuoridorX(('j1','j2'))
-while True:
-    jeu.afficher()
-    jeu.jeu.jouer_coup(2)
-    jeu.afficher()
-    jeu.jeu.jouer_coup(1)
+
+if __name__ == "__main__":
+    jeu = QuoridorX(('j1', 'j2'))
+    while True:
+        jeu.afficher()
+        jeu.jouer_coup(2)
+        jeu.afficher()
+        jeu.jouer_coup(1)
